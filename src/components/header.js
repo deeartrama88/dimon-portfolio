9a4981cb5a2d1215bd265cec71fb289f815e0605
Deeartrama <deeartrama88>
2018-11-11 18:32:31 +0200

    small header styles fixes

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,17 @@ const Header = () => {
          <div className="name">
             <img src={Avatar} className="avatar" alt="avatar"/>
             <div className="name_and_prof">
-               <div className="fullName"><green>T</green>arasenko <green>D</green>mitriy</div>
-               <div className="prof"><span className="mobNewLine"><green>Game</green> developer </span> / <green>Lead</green> 3d artist</div>
+               <div className="fullName">
+                  <span className="mobNewLine">
+                     <span className="header_green">T</span>arasenko
+                  </span>
+                  <span className="header_green"> D</span>mitriy</div>
+               <div className="prof">
+                  <span className="mobNewLine">
+                     <span className="header_green">Game</span> developer
+                  </span> /
+                     <span className="header_green"> Lead</span> 3d artist
+               </div>
             </div>
          </div>
          <div className="menu">
