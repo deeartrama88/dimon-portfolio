00fac952f6b38d88ac3d6c6f4f8e48e52595de9a
Deeartrama <deeartrama88>
2019-05-19 01:16:15 +0300

    header remake

--- src/components/header.js
+++ src/components/header.js
@@ -5,27 +5,23 @@ const Header = () => {
    return (
       <header>
          <div className="name">
-            <img src={Avatar} className="avatar" alt="avatar"/>
+            {/*<img src={Avatar} className="avatar" alt="avatar"/>*/}
             <div className="name_and_prof">
-               <div className="fullName">
-                  <span className="mobNewLine">
-                     <span className="header_green">T</span>arasenko
-                  </span>
-                  <span className="header_green"> D</span>mitriy</div>
+               <div className="fullName">Tarasenko Dmitriy</div>
                <div className="prof">
-                  <span className="mobNewLine">
-                     <span className="header_green">Game</span> developer
-                  </span> /
-                     <span className="header_green"> Lead</span> 3d artist
+                  Lead 3D artist
+                  <span> / </span>
+                  Technical Artist
+                  <span> / </span>
+                  Game developer
                </div>
             </div>
          </div>
          <div className="menu">
             <ul>
                <li className="selected">About me</li>
-               <li>Experience</li>
                <li>Portfolio</li>
-               <li>My work</li>
+               <li>Contact</li>
             </ul>
          </div>
       </header>
