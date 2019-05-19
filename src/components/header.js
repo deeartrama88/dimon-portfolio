c24993fe2d28a85a59cb00d47da3136b50f40917
Deeartrama <deeartrama88>
2019-05-19 15:57:39 +0300

    3d model added and slider arrow size fix, autoplay added

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Avatar from '../assets/images/avatar.jpg';
+// import Avatar from '../assets/images/avatar.jpg';
 
 const Header = () => {
    return (
--- src/components/portfolioWorks/wildWest.js
+++ src/components/portfolioWorks/wildWest.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Slider from 'react-slick';
 import Scene1 from '../../assets/images/works/wild-west-scene-1.jpg';
 import Scene2 from '../../assets/images/works/wild-west-scene-2.jpg';
@@ -23,7 +23,6 @@ import Icon15 from '../../assets/images/works/icon-15.png';
 import Icon16 from '../../assets/images/works/icon-16.png';
 import Icon17 from '../../assets/images/works/icon-17.png';
 import Icon18 from '../../assets/images/works/icon-18.png';
-import Characters from '../../assets/images/works/wild-west-characters.jpg';
 import Indiane from '../../assets/images/works/west-set/Indiane.png';
 import Jenifer from '../../assets/images/works/west-set/Jenifer.png';
 import Mexican from '../../assets/images/works/west-set/Mexican.png';
@@ -32,63 +31,90 @@ import Preacher from '../../assets/images/works/west-set/Preacher.png';
 import Teenager from '../../assets/images/works/west-set/Teenager.png';
 import YoungMan from '../../assets/images/works/west-set/YoungMan.png';
 
-const WildWest = () => {
-   return (
-      <section className="wild-west">
-         <div className="wild-west__title">WILD WEST <br /> PROJECT</div>
-         <div className="wild-west__scenes-one">
-            <div className="container">
-               <div className="flex-wrap">
-                  <div className="wild-west__desc">
-                     Project for mobile on Unity, creating and optimization low poly
-                     content, scene creating, assets and shaders setup, character
-                     animations setup(state machine)
-                  </div>
-                  <img src={Scene1} alt="scene screen shot"/>
-                  <img src={Scene2} alt="scene screen shot"/>
-               </div>
-            </div>
+class WildWest extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         modelWidth: this.getResponsiveSize().width,
+         modelHeight: this.getResponsiveSize().height,
+      };
+      console.log(window.innerWidth);
+   }
 
-         </div>
-         <div className="wild-west__scenes-two">
-            <div className="container">
-               <img src={Scene3} alt="scene screen shot"/>
+   getResponsiveSize = () => {
+      let size = {
+         width: 320,
+         height: 180,
+      };
+      const width = window.innerWidth;
+      if (width > 550) {
+         size.width = 440;
+         size.height = 280;
+      }
+      if (width > 850) {
+         size.width = 640;
+         size.height = 480;
+      }
+      return size;
+   };
+
+   render() {
+      return (
+         <section className="wild-west">
+            <div className="wild-west__title">WILD WEST <br /> PROJECT</div>
+            <div className="wild-west__scenes-one">
+               <div className="container">
+                  <div className="flex-wrap">
+                     <div className="wild-west__desc">
+                        Project for mobile on Unity, creating and optimization low poly
+                        content, scene creating, assets and shaders setup, character
+                        animations setup(state machine)
+                     </div>
+                     <img src={Scene1} alt="scene screen shot"/>
+                     <img src={Scene2} alt="scene screen shot"/>
+                  </div>
+               </div>
+
             </div>
-         </div>
-         <div className="wild-west__weapons">
-            <div className="wild-west__weapons__title">Low poly weapon for characters, screenshots from unity.</div>
-            <div className="wild-west__weapons-wrap">
-               <div className="flex-container container">
-                  <img src={Weapon1} alt="weapons"/>
-                  <img src={Weapon2} alt="weapons"/>
+            <div className="wild-west__scenes-two">
+               <div className="container">
+                  <img src={Scene3} alt="scene screen shot"/>
                </div>
             </div>
-         </div>
-         <div className="wild-west__icons">
-            <div className="container">
-               <div className="wild-west__icons-item">Render 3d models for icons.</div>
-               <div className="slider--wrapper">
-                  <Slider
-                     centerMode={false}
-                     dots={true}
-                     infinite={true}
-                     speed={500}
-                     slidesToShow={5}
-                     slidesToScroll={1}
-                     responsive={[
-                        {
-                           breakpoint: 900,
-                           settings: {
-                              slidesToShow: 3,
-                           }
-                        },
-                        {
-                           breakpoint: 520,
-                           settings: {
-                              slidesToShow: 1,
-                           }
-                        }]}
-                  >
+            <div className="wild-west__weapons">
+               <div className="wild-west__weapons__title">Low poly weapon for characters, screenshots from unity.</div>
+               <div className="wild-west__weapons-wrap">
+                  <div className="flex-container container">
+                     <img src={Weapon1} alt="weapons"/>
+                     <img src={Weapon2} alt="weapons"/>
+                  </div>
+               </div>
+            </div>
+            <div className="wild-west__icons">
+               <div className="container">
+                  <div className="wild-west__icons-item">Render 3d models for icons.</div>
+                  <div className="slider--wrapper">
+                     <Slider
+                        dots={true}
+                        autoplay={true}
+                        infinite={true}
+                        speed={1000}
+                        slidesToShow={5}
+                        slidesToScroll={1}
+                        responsive={[
+                           {
+                              breakpoint: 900,
+                              settings: {
+                                 slidesToShow: 3,
+                              }
+                           },
+                           {
+                              breakpoint: 520,
+                              settings: {
+                                 slidesToShow: 1,
+                              }
+                           }]}
+                     >
                         <img className="wild-west__icons-item" src={Icon1} alt="icon"/>
                         <img className="wild-west__icons-item" src={Icon2} alt="icon"/>
                         <img className="wild-west__icons-item" src={Icon3} alt="icon"/>
@@ -107,60 +133,76 @@ const WildWest = () => {
                         <img className="wild-west__icons-item" src={Icon16} alt="icon"/>
                         <img className="wild-west__icons-item" src={Icon17} alt="icon"/>
                         <img className="wild-west__icons-item" src={Icon18} alt="icon"/>
-                  </Slider>
+                     </Slider>
+                  </div>
+                  <div className="wild-west__model">
+                     <hr />
+                     <div className="sketchfab-embed-wrapper">
+                        <iframe
+                           width={this.state.modelWidth}
+                           height={this.state.modelHeight}
+                           src="https://sketchfab.com/models/85bfdfed42c3464ea2b8321e09462ca3/embed"
+                           frameBorder="0"
+                           title={'model'}
+                           allow="autoplay; fullscreen; vr"
+                           mozallowfullscreen="true"
+                           webkitallowfullscreen="true">
+                        </iframe>
+                     </div>
+                  </div>
                </div>
             </div>
-         </div>
 
-         <div className="wild-west__characters">
-            <div className="container">
-               <div className="wild-west__characters-title">Low Poly characters, diffuse only</div>
-               <div className="slider--wrapper">
-                  <Slider
-                     centerMode={true}
-                     dots={true}
-                     infinite={true}
-                     speed={500}
-                     slidesToShow={3}
-                     slidesToScroll={1}
-                     responsive={[
-                        {
-                           breakpoint: 900,
-                           settings: {
-                              slidesToShow: 1,
-                              slidesToScroll: 1,
-                              centerMode: false,
-                           }
-                        }]}
-                  >
-                     <div className={'slider-item'}>
-                        <img src={Indiane} alt="carousel image"/>
-                     </div>
-                     <div className={'slider-item'}>
-                        <img src={Jenifer} alt="carousel image"/>
-                     </div>
-                     <div className={'slider-item'}>
-                        <img src={Mexican} alt="carousel image"/>
-                     </div>
-                     <div className={'slider-item'}>
-                        <img src={Niger} alt="carousel image"/>
-                     </div>
-                     <div className={'slider-item'}>
-                        <img src={Preacher} alt="carousel image"/>
-                     </div>
-                     <div className={'slider-item'}>
-                        <img src={Teenager} alt="carousel image"/>
-                     </div>
-                     <div className={'slider-item'}>
-                        <img src={YoungMan} alt="carousel image"/>
-                     </div>
-                  </Slider>
+            <div className="wild-west__characters">
+               <div className="container">
+                  <div className="wild-west__characters-title">Low Poly characters, diffuse only</div>
+                  <div className="slider--wrapper">
+                     <Slider
+                        centerMode={true}
+                        dots={true}
+                        autoplay={true}
+                        infinite={true}
+                        speed={1000}
+                        slidesToShow={3}
+                        slidesToScroll={1}
+                        responsive={[
+                           {
+                              breakpoint: 900,
+                              settings: {
+                                 slidesToShow: 1,
+                                 slidesToScroll: 1,
+                                 centerMode: false,
+                              }
+                           }]}
+                     >
+                        <div className={'slider-item'}>
+                           <img src={Indiane} alt="carousel"/>
+                        </div>
+                        <div className={'slider-item'}>
+                           <img src={Jenifer} alt="carousel"/>
+                        </div>
+                        <div className={'slider-item'}>
+                           <img src={Mexican} alt="carousel"/>
+                        </div>
+                        <div className={'slider-item'}>
+                           <img src={Niger} alt="carousel"/>
+                        </div>
+                        <div className={'slider-item'}>
+                           <img src={Preacher} alt="carousel"/>
+                        </div>
+                        <div className={'slider-item'}>
+                           <img src={Teenager} alt="carousel"/>
+                        </div>
+                        <div className={'slider-item'}>
+                           <img src={YoungMan} alt="carousel"/>
+                        </div>
+                     </Slider>
+                  </div>
                </div>
             </div>
-         </div>
-
-      </section>
-   )
+         </section>
+      )
+   }
 };
 
 export default WildWest;
